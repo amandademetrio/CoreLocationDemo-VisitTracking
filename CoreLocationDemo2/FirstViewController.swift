7b6771e41c85400a7957e9515ebfe23455e2623f
amandademetrio <amandademetrio@gmail.com>
2018-09-13 18:24:42 -0700

    Initial Commit

---
+++ CoreLocationDemo2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CoreLocationDemo2
+//
+//  Created by Amanda Demetrio on 9/13/18.
+//  Copyright © 2018 Amanda Demetrio. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CoreLocationDemo2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CoreLocationDemo2
+//
+//  Created by Amanda Demetrio on 9/13/18.
+//  Copyright © 2018 Amanda Demetrio. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
